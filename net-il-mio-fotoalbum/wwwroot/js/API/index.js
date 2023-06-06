4180eb0ac3fe63e70ea043401271b75e11a019ad
codingcele <marcocelegato9@gmail.com>
2023-06-06 10:15:39 +0200

    authorization nelle views

--- net-il-mio-fotoalbum/wwwroot/js/API/index.js
+++ net-il-mio-fotoalbum/wwwroot/js/API/index.js
@@ -1,5 +1,4 @@
-﻿
-function loadImages(searchKey) {
+﻿function loadImages(searchKey, isAdmin) {
     axios.get('/Home', {
         params: {
             str: searchKey
@@ -7,6 +6,7 @@ function loadImages(searchKey) {
     })
         .then((res) => {        //se la richiesta va a buon fine
             console.log('risposta ok', res);
+            console.log("isAdmin:", isAdmin);
             if (res.data.length == 0) {     //non ci sono post da mostrare => nascondo la tabella
                 document.getElementById('images').classList.add('d-none');
                 document.getElementById('filtered-images').classList.add('d-none');
@@ -20,48 +20,95 @@ function loadImages(searchKey) {
                 document.getElementById('filtered-images').innerHTML = '';
                 res.data.forEach(image => {
                     console.log('image', image);
-                    document.getElementById('filtered-images').innerHTML +=
-                        `
-                            <div class="row mb-3">
-                                <div class="col d-flex align-items-center fs-4 pb-2">
-                                    <div class="col-3">
-                                        <img src="./img/${image.picture}" style="max-width: 250px; max-height: 170px;" />
-                                    </div> 
-                                    <div class="col-2 text-center">
-                                        ${image.title}
-                                    </div>
-                                    <div class="col-4 text-center">
-                                        ${image.categories.map(category => {
-                                            if (image.categories.indexOf(category) !== image.categories.length - 1) {
-                                                return `<span> ${category.name},</span>`;
-                                            } else if (image.categories.length !== 1) {
-                                                return `<span> ${category.name}.</span>`;
-                                            } else {
-                                                return `<span> ${category.name}</span>`;
-                                            }
-                                        }).join('')}
-                                    </div>
-                                    <div class="col-3 text-center pl-4">
-                                        <a href="/Home/Update?id=${image.id}" class="btn" style="background-color:green; color:white;">Modifica</a>
-                                        <a href="/Home/Details?id=${image.id}" class="btn btn-primary">Dettagli</a>
-                
-                                        <form action="/Home/Delete/${image.id}" method="post">
-                                            <input type="hidden" name="_method" value="delete">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <button type="submit" class="btn btn-danger btn-sm">
-                                                Elimina
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        `
-                })
+
+                    // Creazione dell'elemento div
+                    var rowDiv = document.createElement('div');
+                    rowDiv.classList.add('row', 'mb-3');
+
+                    // Creazione dell'elemento interno al div
+                    var colDiv = document.createElement('div');
+                    colDiv.classList.add('col', 'd-flex', 'align-items-center', 'fs-4', 'pb-2');
+
+                    // Aggiunta del primo elemento col-3
+                    var imgDiv = document.createElement('div');
+                    imgDiv.classList.add('col-3');
+                    var imageElement = document.createElement('img');
+                    imageElement.src = `./img/${image.picture}`;
+                    imageElement.style.maxWidth = '250px';
+                    imageElement.style.maxHeight = '170px';
+                    imgDiv.appendChild(imageElement);
+                    colDiv.appendChild(imgDiv);
+
+                    // Aggiunta del secondo elemento col-2
+                    var titleDiv = document.createElement('div');
+                    titleDiv.classList.add('col-2', 'text-center');
+                    titleDiv.textContent = image.title;
+                    colDiv.appendChild(titleDiv);
+
+                    // Aggiunta del terzo elemento col-4
+                    var categoriesDiv = document.createElement('div');
+                    categoriesDiv.classList.add('col-4', 'text-center');
+                    image.categories.forEach((category, index) => {
+                        var categorySpan = document.createElement('span');
+                        categorySpan.textContent = category.name;
+                        if (index !== image.categories.length - 1) {
+                            categorySpan.textContent += ',';
+                        }
+                        categoriesDiv.appendChild(categorySpan);
+                    });
+                    colDiv.appendChild(categoriesDiv);
+
+                    // Aggiunta del quarto elemento col-3
+                    var actionsDiv = document.createElement('div');
+                    actionsDiv.classList.add('col-3', 'text-center', 'pl-4');
+                    if (isAdmin) {
+                        var updateLink = document.createElement('a');
+                        updateLink.href = `/Home/Update?id=${image.id}`;
+                        updateLink.classList.add('btn');
+                        updateLink.style.backgroundColor = 'green';
+                        updateLink.style.color = 'white';
+                        updateLink.textContent = 'Modifica';
+                        actionsDiv.appendChild(updateLink);
+                    }
+                    var detailsLink = document.createElement('a');
+                    detailsLink.href = `/Home/Details?id=${image.id}`;
+                    detailsLink.classList.add('btn', 'btn-primary');
+                    detailsLink.textContent = 'Dettagli';
+                    actionsDiv.appendChild(detailsLink);
+                    if (isAdmin) {
+                        var deleteForm = document.createElement('form');
+                        deleteForm.action = `/Home/Delete/${image.id}`;
+                        deleteForm.method = 'post';
+
+                        var hiddenMethodInput = document.createElement('input');
+                        hiddenMethodInput.type = 'hidden';
+                        hiddenMethodInput.name = '_method';
+                        hiddenMethodInput.value = 'delete';
+                        deleteForm.appendChild(hiddenMethodInput);
+
+                        var hiddenTokenInput = document.createElement('input');
+                        hiddenTokenInput.type = 'hidden';
+                        hiddenTokenInput.name = '_token';
+                        hiddenTokenInput.value = '{{ csrf_token() }}';
+                        deleteForm.appendChild(hiddenTokenInput);
+
+                        var deleteButton = document.createElement('button');
+                        deleteButton.type = 'submit';
+                        deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+                        deleteButton.textContent = 'Elimina';
+                        deleteForm.appendChild(deleteButton);
+
+                        actionsDiv.appendChild(deleteForm);
+                    }
+                    colDiv.appendChild(actionsDiv);
+
+                    rowDiv.appendChild(colDiv);
+                    document.getElementById('filtered-images').appendChild(rowDiv);
+                });
             }
         })
         .catch((res) => {       //se la richiesta non è andata a buon fine
             console.error('errore', res);
             alert('errore lista immagini');
         });
-
 }
