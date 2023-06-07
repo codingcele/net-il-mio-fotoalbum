caa76c5cbbbc4fc3ede4dc0ad1efbe4caf34c845
codingcele <marcocelegato9@gmail.com>
2023-06-07 18:48:36 +0200

    gestione categorie (Index)

---
+++ net-il-mio-fotoalbum/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace net_il_mio_fotoalbum
+{
+    [Authorize(Roles = "ADMIN")]
+    public class CategoriesController : Controller
+    {
+        private readonly AlbumContext _context;
+
+        public CategoriesController(AlbumContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories;
+            categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
