e5d23d6b1cc9f0f12c63977656b8e9f1578b48e9
codingcele <marcocelegato9@gmail.com>
2023-06-07 20:54:31 +0200

    update categorie

--- net-il-mio-fotoalbum/Controllers/CategoriesController.cs
+++ net-il-mio-fotoalbum/Controllers/CategoriesController.cs
@@ -41,5 +41,54 @@ namespace net_il_mio_fotoalbum
                 return NotFound();
             }
         }
+
+        [Authorize(Roles = "ADMIN")]
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            Category categoryToEdit = _context.Categories.Where(cat => cat.Id == id).FirstOrDefault();
+
+            if (categoryToEdit == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                CategoryFormModel model = new CategoryFormModel();
+
+                //model.Id = categoryToEdit.Id;
+                model.Name = categoryToEdit.Name;
+                model.Description = categoryToEdit.Description;
+
+                return View(model);
+            }
+        }
+
+        [Authorize(Roles = "ADMIN")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, CategoryFormModel data)
+        {
+            Category categoryToEdit = _context.Categories.Where(img => img.Id == id).FirstOrDefault();
+
+            if (!ModelState.IsValid)
+            {
+                return View("Update", data);
+            }
+
+            if (categoryToEdit != null)
+            {
+                categoryToEdit.Name = data.Name;
+                categoryToEdit.Description = data.Description;
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
---
+++ net-il-mio-fotoalbum/Models/CategoryFormModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Build.Framework;
+
+namespace net_il_mio_fotoalbum
+{
+    public class CategoryFormModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
